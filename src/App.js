f2a9d41f3445f4f85d76b356f34391bef83f3725
unnikrishnansk <unni.sk94@gmail.com>
2022-04-23 09:56:27 +0530

    counter_assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Counter from "./components/Counter";
+import {useState} from 'react';
 
 function App() {
+
+  let [count,setCount] = useState(0);
+
+  let addCount = () => {
+    setCount(count+1);
+  }
+
+  let subCount = () => {
+    setCount(count-1);
+  }
+
+  let doubleCount = () => {
+    setCount(count*2);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter count={count}/>
+
+      <button onClick={addCount}>Add</button>
+      <button onClick={subCount}>Subtract</button>
+      <button onClick={doubleCount}>Double</button>
     </div>
   );
 }
